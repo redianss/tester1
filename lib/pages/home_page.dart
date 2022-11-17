0c9cc39d0fbfb1c45c7f1d61ebecab769f0602af
redianss <rjanuar5@gmail.com>
2022-11-17 16:40:09 +0700

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tester1/read%20data/get_user_name.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,7 +18,11 @@ class _HomePageState extends State<HomePage> {
 
   // get documen id
   Future getDocID() async {
-    await FirebaseFirestore.instance.collection('users').get().then(
+    await FirebaseFirestore.instance
+        .collection('users')
+        .orderBy('age', descending: false)
+        .get()
+        .then(
           (snapshoot) => snapshoot.docs.forEach((document) {
             print(document.reference);
             docID.add(document.reference.id);
@@ -34,18 +39,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          user.email!,
+          style: TextStyle(fontSize: 16),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: Icon(Icons.logout),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Sign In as :' + user.email!),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.amberAccent[200],
-              child: Text('Sign Out'),
-            ),
             Expanded(
               child: FutureBuilder(
                 future: getDocID(),
@@ -53,8 +64,12 @@ class _HomePageState extends State<HomePage> {
                   return ListView.builder(
                     itemCount: docID.length,
                     itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(docID[index]),
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          title: GetUserName(documentId: docID[index]),
+                          tileColor: Colors.blueAccent[100],
+                        ),
                       );
                     },
                   );

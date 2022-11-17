2f5f61ad185816c14a8b601e4c4be9eb8e4348e7
redianss <rjanuar5@gmail.com>
2022-11-17 15:03:21 +0700

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,23 +12,58 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  // documen id
+  List<String> docID = [];
+
+  // get documen id
+  Future getDocID() async {
+    await FirebaseFirestore.instance.collection('users').get().then(
+          (snapshoot) => snapshoot.docs.forEach((document) {
+            print(document.reference);
+            docID.add(document.reference.id);
+          }),
+        );
+  }
+
+  @override
+  void initState() {
+    getDocID();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Sign In as :' + user.email!),
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            color: Colors.amberAccent[200],
-            child: Text('Sign Out'),
-          )
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Sign In as :' + user.email!),
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: Colors.amberAccent[200],
+              child: Text('Sign Out'),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                future: getDocID(),
+                builder: (context, snapshot) {
+                  return ListView.builder(
+                    itemCount: docID.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(docID[index]),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
